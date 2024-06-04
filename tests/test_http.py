ada80cea250094eb45d84a6154dca21f885c647a
NadiiaMa <n.maklyak@yahoo.com>
2024-06-05 00:00:17 +0200

    lost in commits

--- tests/test_http.py
+++ tests/test_http.py
@@ -7,8 +7,8 @@ def test_first_request():
     r = requests.get('https://api.github.com/zen')
     print(f"Response is {r.text}")
 
+
 @pytest.mark.http
 def test_second_request():
     r = requests.get('https://api.github.com/users/defunkt')
     print(f"Response Body is {r.text}")
-
