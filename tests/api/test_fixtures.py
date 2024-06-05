a1f00ecdb89755311ad95153c6c86f2f39f33945
NadiiaMa <n.maklyak@yahoo.com>
2024-06-05 12:50:52 +0200

    17.2_

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -9,7 +9,3 @@ def test_change_name(user):
 @pytest.mark.check
 def test_change_second_name(user):
     assert user.second_name == 'Makliak'
-<<<<<<< HEAD
-=======
-              
->>>>>>> b2d02cb (lost in commits)
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -11,3 +11,4 @@ def test_user_exists(github_api):
 def test_user_not_exists(github_api):
     r = github_api.get_user('butenkosergii')
     assert r['message'] == 'Not Found'
+    
